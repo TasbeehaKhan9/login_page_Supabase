8cbbdbd548292c813ae54ae99c03fb08b324138c
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2026-01-07 23:57:51 +0500

    upload

--- class 2/db.js
+++ class 2/db.js
@@ -7,16 +7,16 @@ let btn = document.getElementById('btn');
 let todoBtn = document.getElementById('addTodo');
 let prior = document.getElementsByName('priority');
 let main = document.getElementById('main');
+let editId = null;
 
 async function addTodo(e){
     e.preventDefault()
-    console.log(editId);
 
     let selectedPrio;
     console.log(title.value,desc.value)
     for(var p of prior){
         if(p.checked){
-            selectedPrio = p.value
+            selectedPrio = p.value;
         }
     }
 
@@ -27,11 +27,13 @@ async function addTodo(e){
   .eq('id', editId)
   if(error){
     console.log('error in updating data',error);
+    alert("todo updated succesfully")
+    editId = null;
 
   }else{
     alert("it is updates Succesfully")
     allTodos()
-  }
+  
 
    try{
    const { error } = await supabase
@@ -51,6 +53,81 @@ async function addTodo(e){
         
     }
 
-  
+  }
+  allTodos();
     }
+}
+
+
+btn.addEventListener("submit", addTodo)
+
+async function allTodos(){
+
+  try {
+    const { data,error } = await supabase
+  .from('todos')
+  .select("*")
+  if(data){
+    showAllTodos(data)
+    
+  }else{
+
+  }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+allTodos()
+async function showAllTodos(todos){
+  console.log(todos);
+  main.innerHTML = "";
+  todos.forEach(todo => {
+    main.innerHTML += `<div class="card">
+  <div class="card-body">
+    ${todo.title}
+  </div>
+   <div class="card-body">
+    ${todo.priority}
+  </div>
+   <div class="card-body">
+    ${todo.description}
+  </div>
+  <div><button class='btn' onclick='edtTodo(${todo.id},"${todo.title}","${todo.description}","${todo.priority}")'>🖊️</button></div> 
+   <div><button class='btn' onclick='dltTodo(${todo.id})'>🗑️</button></div> 
+ 
+ 
+</div>` 
+  });
+}
+window.edtTodo = (id,tit,desc,prior)=>{
+  console.log(id,tit,desc,prior);
+  title.value = tit;
+  description.value = desc;
+  priority.forEach( p =>{
+    console.log(P);
+    
+    p.checked = p.value === priority
+  })
+
+todoBtn.innerHTML = " Edit todo"
+editId = id
+
+}
+
+
+window.dltTodo =  async (id)=>{
+  try {
+const response = await supabase
+  .from('todos')
+  .delete()
+  .eq('id', id)
+  if(response){
+    console.log(response);
+    allTodos()
+  }
+  } catch (error) {
+    console.log(error);
+  }
 }
